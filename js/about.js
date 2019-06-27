dcb7bf5deadd0cf48bfdfdacadd26d3845c25f85
ginapult <gina.pultorak@gmail.com>
2019-06-27 13:13:48 -0700

    click projects

--- js/about.js
+++ js/about.js
@@ -1,3 +1,10 @@
 'use strict';
 
 //Global variables
+
+var flipContainer = document.getElementsByClassName('flip-container');
+
+flipContainer.onclick = function () {
+  this.classList.toggle('flipped');
+  console.log('i was clicked.');
+};
